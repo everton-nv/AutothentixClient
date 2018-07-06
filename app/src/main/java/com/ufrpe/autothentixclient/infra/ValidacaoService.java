76297b6868e7439c320ae7ed48317b69b95439e7
Unknown <jadieleudes@gmail.com>
2018-07-05 23:23:17 -0300

    Função de validar data

--- app/src/main/java/com/ufrpe/autothentixclient/infra/ValidacaoService.java
+++ app/src/main/java/com/ufrpe/autothentixclient/infra/ValidacaoService.java
@@ -2,28 +2,37 @@ package com.ufrpe.autothentixclient.infra;
 
 import android.text.TextUtils;
 import android.util.Patterns;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class ValidacaoService {
+    private static final String DATA_COMUM   = "dd/MM/yyyy";
 
-    public boolean isCampoVazio(String valor) {
+    private static final int TAMCPF = 11;
+    private static final int TAMCNPJ = 14;
+    private static final int TAMTEL = 11;
+    private static final int ZERO = 0;
+    private static final int TAMANHO_DATA = 10;
+
+    static boolean isCampoVazio(String valor) {
 
         return (TextUtils.isEmpty(valor) || valor.trim().isEmpty());
     }
 
-    public boolean isEmail(String email) {
+    public boolean isEmailValido(String email) {
 
         return (!(isCampoVazio(email)) || Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
-    public boolean isTelefone(String telefone) {
+    public boolean isTelefoneValido(String telefone) {
 
-        return (!(isCampoVazio(telefone)) || telefone.length() == 11);
+        return (!(isCampoVazio(telefone)) || telefone.length() == TAMTEL);
     }
 
-    public boolean isCpf(String cpf) {
+    public boolean isCpfValido(String cpf) {
 
-        return (!(isCampoVazio(cpf)) || cpf.length() == 11);
+        return (!(isCampoVazio(cpf)) || cpf.length() == TAMCPF);
     }
 
     public boolean isSenhaValida(String senha) {
@@ -40,8 +49,42 @@ public class ValidacaoService {
         return senha.equals(confirmaSenha);
     }
 
-    public boolean isCnpj(String cnpj) {
+    public boolean isCnpjValido(String cnpj) {
 
-        return (!(isCampoVazio(cnpj)) || cnpj.length() == 14);
+        return (!(isCampoVazio(cnpj)) || cnpj.length() == TAMCNPJ);
+    }
+
+    static boolean dataMenorOuIgualQueAtual(String data) {
+        SimpleDateFormat dataFormatada = new SimpleDateFormat(DATA_COMUM);
+        dataFormatada.setLenient(false);
+        //Testa no formato dd/MM/yyyy
+        try {
+            Date dataAtual = new Date();
+            Date dataCliente = dataFormatada.parse(data);
+
+            if (dataAtual.compareTo(dataCliente) >= ZERO) {
+                return true;
+            }
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    static boolean dataExiste(String data) {
+        SimpleDateFormat dataFormatada = new SimpleDateFormat(DATA_COMUM);
+        dataFormatada.setLenient(false);
+
+        //Testa no formato dd/MM/yyyy
+        try {
+            dataFormatada.parse(data);
+            return true;
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    public boolean isDataValida(String data){
+
+        return(dataExiste(data) && dataMenorOuIgualQueAtual(data) && data.length() == TAMANHO_DATA);
     }
 }
