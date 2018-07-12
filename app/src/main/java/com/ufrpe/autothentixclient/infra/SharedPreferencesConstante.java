7215914fcd63eb82d75244b809c20f642a46d32e
Edson Fagner <pg2006pe@hotmail.com>
2018-07-11 21:03:33 -0300

    Shared Preferences (Classe de serviço e Constantes).

---
+++ app/src/main/java/com/ufrpe/autothentixclient/infra/SharedPreferencesConstante.java
@@ -0,0 +1,12 @@
+package com.ufrpe.autothentixclient.infra;
+
+public class SharedPreferencesConstante {
+    public static final String TITLE_PREFERENCES        = "preferencias";
+    public static final String LOGIN_PREFERENCES        = "login";
+    public static final String PASSWORD_PREFERENCES     = "senha";
+    public static final String ID_USER_PREFERENCES        = "id_usuario";
+
+    public static final String DEFAULT_LOGIN_PREFERENCES        = "";
+    public static final String DEFAULT_PASSWORD_PREFERENCES     = "";
+    public static final long DEFAULT_ID_USER_PREFERENCES = 0;
+}
---
+++ app/src/main/java/com/ufrpe/autothentixclient/infra/SharedPreferencesServices.java
@@ -0,0 +1,54 @@
+package com.ufrpe.autothentixclient.infra;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static com.ufrpe.autothentixclient.infra.SharedPreferencesConstante.DEFAULT_ID_USER_PREFERENCES;
+import static com.ufrpe.autothentixclient.infra.SharedPreferencesConstante.DEFAULT_LOGIN_PREFERENCES;
+import static com.ufrpe.autothentixclient.infra.SharedPreferencesConstante.DEFAULT_PASSWORD_PREFERENCES;
+import static com.ufrpe.autothentixclient.infra.SharedPreferencesConstante.ID_USER_PREFERENCES;
+import static com.ufrpe.autothentixclient.infra.SharedPreferencesConstante.LOGIN_PREFERENCES;
+import static com.ufrpe.autothentixclient.infra.SharedPreferencesConstante.PASSWORD_PREFERENCES;
+import static com.ufrpe.autothentixclient.infra.SharedPreferencesConstante.TITLE_PREFERENCES;
+
+public class SharedPreferencesServices {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    public SharedPreferencesServices(Context context) {
+        sharedPreferences = context.getSharedPreferences(TITLE_PREFERENCES, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void setIdUserPreferences(long idUser){
+        editor.putLong(ID_USER_PREFERENCES, idUser);
+        editor.commit();
+    }
+
+    public void setLoginPreferences(String login){
+        editor.putString(LOGIN_PREFERENCES, login);
+        editor.commit();
+    }
+
+    public void setPasswordPreferences(String password){
+        editor.putString(PASSWORD_PREFERENCES, password);
+        editor.commit();
+    }
+
+    public long getIdUserPreferences(){
+        return sharedPreferences.getLong(ID_USER_PREFERENCES, DEFAULT_ID_USER_PREFERENCES);
+    }
+
+    public String getLoginPreferences(){
+        return sharedPreferences.getString(LOGIN_PREFERENCES, DEFAULT_LOGIN_PREFERENCES);
+    }
+
+    public String getPasswordPreferences(){
+        return sharedPreferences.getString(PASSWORD_PREFERENCES, DEFAULT_PASSWORD_PREFERENCES);
+    }
+
+    public void clearPreferences(){
+        editor.clear();
+        editor.commit();
+    }
+}
