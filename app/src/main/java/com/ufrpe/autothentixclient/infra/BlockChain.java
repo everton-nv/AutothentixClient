4d94a5c86e4ecd10972ab0c3db85a4e4da76d166
Everton <everton.nasc43@gmail.com>
2018-07-26 22:43:32 -0300

    Classe block chain

---
+++ app/src/main/java/com/ufrpe/autothentixclient/infra/BlockChain.java
@@ -0,0 +1,33 @@
+package com.ufrpe.autothentixclient.infra;
+
+import java.util.ArrayList;
+
+public class BlockChain {
+    public static ArrayList<Bloco> blockchain = new ArrayList<Bloco>();
+    public static int difficulty = 5;
+
+    public static Boolean isChainValid() {
+        Bloco currentBlock;
+        Bloco previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        //loop through blockchain to check hashes:
+        for(int i=1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hashAtual.equals(currentBlock.calcularHash()) ){
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hashAtual.equals(currentBlock.hashAnterior) ) {
+                return false;
+            }
+            //check if hash is solved
+            if(!currentBlock.hashAtual.substring( 0, difficulty).equals(hashTarget)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
